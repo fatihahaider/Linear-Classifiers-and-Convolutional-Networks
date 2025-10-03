cfaa2b64070f0cc3ec9a5aa43dcc25d2ef3b0ffb
Fatiha Haider <fatiha@princeton.edu>
2025-10-03 17:53:53 -0400

    latest code at 5:53

--- partB/initial/train.py
+++ partB/initial/train.py
@@ -7,16 +7,16 @@ import copy
 ######################################################
 # Set use_pcode to True to use the provided pyc code
 # for inference, calc_gradient, loss_crossentropy and update_weights
-use_pcode = False
+use_pcode = True
 
 # You can modify the imports of this section to indicate
 # whether to use the provided pyc or your own code for each of the four functions.
 if use_pcode:
     # import the provided pyc implementation
     sys.path += ['pyc_code']
-    from inference_ import inference
-    from calc_gradient_ import calc_gradient
-    from update_weights_ import update_weights
+    from pyc_code.inference_ import inference
+    from pyc_code.calc_gradient_ import calc_gradient
+    from pyc_code.update_weights_ import update_weights
 else:
     # import your own implementation
     from inference import inference
@@ -66,14 +66,14 @@ def train(model, input, label, val_input, val_label, params, numIters, numItersP
     update_params = {"learning_rate": lr,
                      "weight_decay": wd }
 
+    train_accuracies, train_losses = [], []
+    val_accuracies, val_losses = [], []
+
     num_inputs = input.shape[-1]
     loss = np.zeros((numIters,))
 
     num_val = val_input.shape[-1]
-    train_accuracies = []
-    train_losses = []
-    val_accuracies = []
-    val_losses = []
+    
 
     best_accuracy = 0
     best_model = {}
@@ -96,25 +96,35 @@ def train(model, input, label, val_input, val_label, params, numIters, numItersP
         # Optionally,
         #   (1) Monitor the progress of training
         #   (2) Save your learnt model, using ``np.savez(save_file, **model)``
-        bs = params['batch_size']
-        j = np.random.randint(0, num_inputs-bs)
-        batch = input[...,j:j+bs] # random sample of data 
-        batch_labels = label[j:j+bs]
+
+        j = np.random.randint(0, num_inputs-batch_size)
+        batch = input[...,j:j+batch_size] # random sample of data 
+        batch_labels = label[j:j+batch_size]
+
+        #forward
         output, activations = inference(model, batch)
         
-        loss, dv_output = loss_crossentropy(output, batch_labels, {}, True)
+        #loss
+        batch_loss, dv_output = loss_crossentropy(output, batch_labels, {}, True)
     
+        #accuracy
         train_accuracies.append(np.sum(np.array([np.argmax(output[j])==np.argmax(batch_labels[j]) for j in range(batch_size)]))/batch_size)
+        train_losses.append(batch_loss)
 
         val_output, _ = inference(model, val_input)
         val_loss, _ = loss_crossentropy(val_output, val_label, {}, False)
         val_accuracies.append(np.sum(np.array([np.argmax(val_label[j])==np.argmax(val_output[j]) for j in range(len(val_input))]))/len(val_input))
         val_losses.append(val_loss)
         
+        #backprop
         grads = calc_gradient(model, batch, activations, dv_output)
 
-        model= update_weights(model, grads, params)
+        #update 
+        model= update_weights(model, grads, update_params)
         
+        print(f"Iter {i+1}/{numIters} | "
+            f"Train loss {batch_loss:.4f}, acc {train_accuracies:.4f} | "
+            f"Val loss {val_loss:.4f}, acc {val_accuracies:.4f}")
     
     ########
     return model, loss, val_losses, val_accuracies, train_losses, train_accuracies 
